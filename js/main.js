b4a4d6e6cb2e2482423ee4526d39771fbc15372c
lnalmeida <l.n.almeida.ti@gmail.com>
2021-12-20 14:16:22 -0300

    Final

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ const blocks = document.getElementsByClassName('blocks');
 
 for (var i = 0; i < 400; i++) {
      banner.innerHTML += "<div class='blocks'></div>";
-     const duration = Math.random() * 20;
-     blocks[i].style.animationDuration = `${15 + duration}s`;
+     const duration = Math.random() * 10;
+     blocks[i].style.animationDuration = `${5 + duration}s`;
      blocks[i].style.animationDelay = `${duration}s`;
 
 }
